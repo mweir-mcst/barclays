10b487ff89fdaa062e8b40dd8c21cf9c7370b37b
Matthew Weir <matthew.weir@mcvts.org>
2022-05-25 08:45:21 -0400

    feat: update rng

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,19 +1,35 @@
 import React from "react";
 
+const inputStyle = {
+    border: "none",
+    fontSize: "20px",
+    height: "30px",
+    padding: "10px"
+};
+
 const App = function() {
     return (
-        <div style={{backgroundColor: "white"}}>
+        <div style={{
+            fontFamily: '"Lucida Console", "Courier New", monospace',
+            backgroundColor: "#c2e1ff",
+            textAlign: "center"
+        }}>
             <header>
-                <h1 style={{color: "#000000"}}>Random Number Generator</h1>
+                <h1 style={{color: "black"}}>Random Number Generator</h1>
             </header>
 
             <section>
                 <label>From:</label>
                 <br />
-                <input type="number" className="fromNum" />
+                <input type="number" className="fromNum" style={inputStyle} />
                 <br />
+                <div className="space" style={{
+                    margin: "10px"
+                }} />
                 <label>To:</label>
-                <input type="number" className="toNum" />
+                <br />
+                <input type="number" className="toNum" style={inputStyle} />
+                <br />
                 <button type="submit" className="button" onClick={function() {
                     const fromNum = document.querySelector(".fromNum").value;
                     const toNum = document.querySelector(".toNum").value;
@@ -24,6 +40,15 @@ const App = function() {
                         const num = document.querySelector(".num");
                         num.innerText = randomNum;
                     }
+                }} style={{
+                    display: "inline-block",
+                    borderRadius: "4px",
+                    backgroundColor: "lightsteelblue",
+                    border: "none",
+                    color: "steelblue",
+                    padding: "5px 7px",
+                    margin: "15px",
+                    fontFamily: '"Lucida Console", "Courier New", monospace'
                 }}>Generate</button>
             </section>
 
